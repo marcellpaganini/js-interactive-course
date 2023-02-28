05a3a970276f30aa39c09004ecada1cac020b7b1
Marcell Tanure <mtanure@waldale.com>
2023-02-28 11:43:10 -0400

    getting tab url and saving it to localStorage

--- ChromeExtension/index.js
+++ ChromeExtension/index.js
@@ -1,18 +1,42 @@
 let myLeads = [];
 const inputEl = document.getElementById('input-el');
 const ulEl = document.getElementById('ul-el');
+const inputBtn = document.getElementById("input-btn");
+const deleteBtn = document.getElementById("delete-btn");
+const tabBtn = document.getElementById("tab-btn");
 // saveLead = () => {
 //     console.log('Btn clicked from onclick attribute');
 // }
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderElements();
+    renderElements(myLeads);
 }
 
-const inputBtn = document.getElementById("input-btn");
+// const tabs = [
+//     {url: "https://linkedin.com/in/per-harald-borgen/"}
+// ]
+
+tabBtn.addEventListener("click", () => {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
+        myLeads.push(tabs[0].url);
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        renderElements(myLeads);
+    })
+})
+
+function renderElements(leads) {
+    let elements = "";
+    for (let index = 0; index < leads.length; index++) {
+        
+        elements += `<li>
+                        <a href='${leads[index]}'>${leads[index]}</a>
+                      </li>`;
+    }
+    ulEl.innerHTML = elements;
+}
 
 inputBtn.addEventListener("click", () => {
     myLeads.push(inputEl.value);
@@ -20,16 +44,11 @@ inputBtn.addEventListener("click", () => {
 
     localStorage.setItem("myLeads", JSON.stringify(myLeads));
 
-    renderElements();
+    renderElements(myLeads);
 });
 
-function renderElements() {
-    let elements = "";
-    for (let index = 0; index < myLeads.length; index++) {
-        
-        elements += `<li>
-                        <a href='${myLeads[index]}'>${myLeads[index]}</a>
-                      </li>`;
-    }
-    ulEl.innerHTML = elements;
-}
+deleteBtn.addEventListener("dblclick", () => {
+    localStorage.clear();
+    myLeads = [];
+    renderElements(myLeads);
+});
